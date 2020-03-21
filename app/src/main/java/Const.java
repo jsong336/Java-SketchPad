c9f4eca7fb8ed2e3f6f66739cbdf1fedf3fa0a8b
jsong336 <jsong336@uwo.ca>
2020-03-21 13:29:14 -0400

    Created development environment, Added Const class, Implemented basic MainView

---
+++ app/src/main/java/Const.java
@@ -0,0 +1,11 @@
+public final class Const {
+    private Const() {} // prevent instantiation
+
+    /** Main constants  */
+    public static final class Main {
+        public static final String TITLE = "Sketch Pad";
+        public static final int HEIGHT = 500;
+        public static final int WIDTH = 500;
+        private Main() {} // prevent instantiation
+    }
+}
